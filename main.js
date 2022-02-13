761fa379248a759d0b54e3f0f24e817714934a8b
Yan3330 <korolevskijan@gmail.com>
2022-02-13 18:45:23 +0300

    Update main.js

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ let house_services = document.getElementById('costs-house-services')
 let transport = document.getElementById('costs-transport')
 let credit = document.getElementById('costs-credit')
 
-let month = document.getElementById('total-month')
-let day = document.getElementById('total-day')
-let year = document.getElementById('total-year')
+let monthInput = document.getElementById('total-month')
+let dayInput = document.getElementById('total-day')
+let yearInput = document.getElementById('total-year')
 
 let totalMonth, totalDay, totalYear;
 
@@ -19,54 +19,42 @@ const moneyBoxRange = document.getElementById('money-box-range')
     totalSpend = document.getElementById('spend')
 
 let accumulation = 0
-let totalPresents = 0
+let totalPrecents = 0
 
 const inputs = document.querySelectorAll('.input')
 
 for (input of inputs) {
     input.addEventListener('input', () => {
-        countingAviableMoney()
-        accumulationMoney()
-        countingSpend()
+        countingAviableMoney();
+        calculationPrecents();
     })
 }
 
+const strToNum = str => str.value ? parseInt(str.value) : 0
+
 const countingAviableMoney = () => {
-    totalMonth.value = (salary.value*1 + freelance.value*1 + extra1.value*1 + extra2.value*1) - (flat.value*1 + house_services.value*1 + transport.value*1 + credit.value*1)
-    countingAviableMoney.value = totalMonth
+    const totalPerMonth = strToNum(salary) + strToNum(freelance) + strToNum(extra1) + strToNum(extra2)
+    const totalCosts = strToNum(flat) + strToNum(house_services) + strToNum(transport) + strToNum(credit)
+    
+    totalMonth = totalPerMonth - totalCosts;
+    monthInput.value = totalMonth;
 }
 
-const accumulationMoney = () => {
-    accumulationMoney.value = month.value*1 * box_range.value*1 / 100
-    accumulationMoney.textContent = accumulationMoney.value
-}
+moneyBoxRange.addEventListener('input', e => {
+    const totalPrecentEl = document.getElementById('total-precents');
+    totalPrecents = e.target.value;
+    totalPrecentEl.innerHTML = totalPrecents;
+    calculationPrecents();
+})
 
-const countingSpend = () => {
-    countingSpend.value = month.value*1 - accumulation.value*1
-    countingSpend.textContent = countingSpend.value
-}
+const calculationPrecents = () => {
+    accumulation = ((totalMonth * totalPrecents) / 100).toFixed();
+    accumulationInput.value = accumulation;
 
-month = function () {
-    let sum = (salary*1 + freelance*1 + extra1*1 + extra2*1) - (flat*1 + house_services*1 + transport*1 + credit*1)
-    value = sum
-}
+    totalSpend.value = totalMonth - accumulation;
+    totalDay = (totalSpend.value / 30).toFixed();
+    dayInput.value = totalDay;
 
-day = function () {
-    let sum = month*1 / 30
-    day.textContent = sum
-}
-
-year = function () {
-    let sum = month*1 * 12
-    year.textContent = sum
-}
-
-accumulation = function() {
-    let precents = month*1 * box_range*1 / 100
-    accumulation.textContent = precents
-}
-
-spend = function() {
-    let sum = month*1 - accumulation*1
-    spend.textContent = sum
+    totalYear = accumulation * 12;
+    yearInput.value = totalYear
 }
